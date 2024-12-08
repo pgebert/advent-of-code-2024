d8af8896684d7ac3ca6a8fe1d7f34188b01c062b
Patrick Gebert <patrick.gebert@web.de>
2024-12-08 14:17:39 +0100

    Added solution for Day08

--- src/main/kotlin/de/pgebert/aoc/days/Day08.kt
+++ src/main/kotlin/de/pgebert/aoc/days/Day08.kt
@@ -1,11 +1,71 @@
 package de.pgebert.aoc.days
 
 import de.pgebert.aoc.Day
-import de.pgebert.aoc.NOT_IMPLEMENTED
 
 class Day08(input: String? = null) : Day(8, "Day8", input) {
 
-    override fun partOne() = NOT_IMPLEMENTED
 
-    override fun partTwo() = NOT_IMPLEMENTED
+    override fun partOne() = buildSet {
+        getAntennaLocations().forEach { locations ->
+            locations.combinations().forEach { (location1, location2) ->
+                calculateAntinodeRecursively(location1, location2, false).firstOrNull()?.let { add(it) }
+                calculateAntinodeRecursively(location2, location1, false).firstOrNull()?.let { add(it) }
+            }
+        }
+    }.size
+
+    override fun partTwo() = buildSet {
+        getAntennaLocations().forEach { locations ->
+            locations.combinations().forEach { (start, end) ->
+                calculateAntinodeRecursively(start, end, true).forEach { add(it) }
+                calculateAntinodeRecursively(end, start, true).forEach { add(it) }
+            }
+        }
+    }.size
+
+
+    private fun getAntennaLocations(): List<List<Pair<Int, Int>>> {
+        val antennas = mutableMapOf<Char, MutableList<Pair<Int, Int>>>()
+
+        inputList.forEachIndexed { i, row ->
+            row.forEachIndexed { j, c ->
+                if (c != '.') {
+                    antennas.getOrPut(c) { mutableListOf() }.add(i to j)
+                }
+            }
+        }
+
+        return antennas.values.toList()
+    }
+
+    private fun Pair<Int, Int>.isWithinBounds() =
+        first in inputList.indices && second in inputList.first().indices
+
+    private fun calculateAntinodeRecursively(
+        start: Pair<Int, Int>,
+        end: Pair<Int, Int>,
+        includeStart: Boolean
+    ): Sequence<Pair<Int, Int>> = sequence {
+        val offsetX = (end.first - start.first)
+        val offsetY = (end.second - start.second)
+
+        var current = start
+
+        while (current.isWithinBounds()) {
+            if (current != start || includeStart) {
+                yield(current)
+            }
+            current = current.first - offsetX to current.second - offsetY
+        }
+    }
+
+    private fun <T> List<T>.combinations(): Sequence<Pair<T, T>> = sequence {
+
+        for (i in indices) {
+            for (j in i + 1 until size) {
+                yield(get(i) to get(j))
+            }
+        }
+    }
+
 }
---
+++ src/test/kotlin/de/pgebert/aoc/Day08Test.kt
@@ -0,0 +1,36 @@
+package de.pgebert.aoc
+
+import de.pgebert.aoc.days.Day08
+import de.pgebert.aoc.utils.shouldBe
+import org.junit.jupiter.api.Test
+
+
+class Day08Test {
+
+    private val example = """
+        ............
+        ........0...
+        .....0......
+        .......0....
+        ....0.......
+        ......A.....
+        ............
+        ............
+        ........A...
+        .........A..
+        ............
+        ............
+    """.trimIndent()
+
+    @Test
+    fun `testing day 02 partOne example`() {
+        val day = Day08(input = example)
+        day.partOne() shouldBe 14
+    }
+
+    @Test
+    fun `testing day 02 partTwo example`() {
+        val day = Day08(input = example)
+        day.partTwo() shouldBe 34
+    }
+}
