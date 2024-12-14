466d03ceb843a8a412e6cbf3d9746f003d067f4a
Patrick Gebert <patrick.gebert@web.de>
2024-12-15 00:45:00 +0100

    Added solution for Day12

--- src/main/kotlin/de/pgebert/aoc/days/Day12.kt
+++ src/main/kotlin/de/pgebert/aoc/days/Day12.kt
@@ -7,33 +7,33 @@ class Day12(input: String? = null) : Day(12, "Day12", input) {
     override fun partOne(): Int {
 
         var result = 0
-
         val visited = mutableSetOf<Pair<Int, Int>>()
 
         for (i in inputList.indices) {
             for (j in inputList[i].indices) {
-
                 if (Pair(i, j) in visited) continue
                 val (area, perimeter) = getAreaAndPerimeter(i to j, visited)
-
                 result += (area * perimeter)
-
-
-                println("${inputList[i][j]},$area,$perimeter, $result")
-
-
             }
         }
 
         return result
     }
 
-    private val directions = listOf(
-        -1 to 0,  // up
-        0 to 1,   // right
-        1 to 0,   // down
-        0 to -1   // left
-    )
+    override fun partTwo(): Int {
+
+        var result = 0
+        val visited = mutableSetOf<Pair<Int, Int>>()
+
+        for (i in inputList.indices) {
+            for (j in inputList[i].indices) {
+                if (Pair(i, j) in visited) continue
+                val (area, perimeter) = getAreaAndPerimeterDiscounted(i to j, visited)
+                result += (area * perimeter)
+            }
+        }
+        return result
+    }
 
 
     private fun getAreaAndPerimeter(pos: Pair<Int, Int>, visited: MutableSet<Pair<Int, Int>>): Pair<Int, Int> {
@@ -44,58 +44,19 @@ class Day12(input: String? = null) : Day(12, "Day12", input) {
 
         directions.forEach { dir ->
             val newPos = Pair(pos.first + dir.first, pos.second + dir.second)
-            if (
-                !newPos.withinBounds()
-                || inputList[newPos.first][newPos.second] != inputList[pos.first][pos.second]
-            ) {
+            if (isSameValue(newPos, pos)) {
+                if (newPos !in visited) {
+                    val (newArea, newPerimeter) = getAreaAndPerimeter(newPos, visited)
+                    areaSize += newArea
+                    perimeterSize += newPerimeter
+                }
+            } else {
                 perimeterSize++
             }
         }
-
-        directions.forEach { dir ->
-            val newPos = Pair(pos.first + dir.first, pos.second + dir.second)
-            if (newPos !in visited
-                && newPos.withinBounds()
-                && inputList[newPos.first][newPos.second] == inputList[pos.first][pos.second]
-            ) {
-                visited.add(newPos)
-                val (newArea, newPerimeter) = getAreaAndPerimeter(newPos, visited)
-                areaSize += newArea
-                perimeterSize += newPerimeter
-            }
-        }
-
         return areaSize to perimeterSize
-
     }
 
-
-    private fun Pair<Int, Int>.withinBounds() =
-        first in inputList.indices && second in inputList[first].indices
-
-    override fun partTwo(): Int {
-
-        var result = 0
-
-        val visited = mutableSetOf<Pair<Int, Int>>()
-
-        for (i in inputList.indices) {
-            for (j in inputList[i].indices) {
-
-                if (Pair(i, j) in visited) continue
-                val (area, perimeter) = getAreaAndPerimeterDiscounted(i to j, visited)
-
-                result += (area * perimeter)
-
-
-                println("${inputList[i][j]},$area,$perimeter, $result")
-
-
-            }
-        }
-
-        return result
-    }
 
     private fun getAreaAndPerimeterDiscounted(
         pos: Pair<Int, Int>,
@@ -109,35 +70,23 @@ class Day12(input: String? = null) : Day(12, "Day12", input) {
         directions.plus(directions.first()).zipWithNext().forEach { (dir1, dir2) ->
             val newPos1 = Pair(pos.first + dir1.first, pos.second + dir1.second)
             val newPos2 = Pair(pos.first + dir2.first, pos.second + dir2.second)
-
             val newPos3 = Pair(pos.first + dir1.first + dir2.first, pos.second + dir1.second + dir2.second)
 
-            // outer corner
-            if ((!newPos1.withinBounds() || inputList[newPos1.first][newPos1.second] != inputList[pos.first][pos.second])
-                && (!newPos2.withinBounds() || inputList[newPos2.first][newPos2.second] != inputList[pos.first][pos.second])
-            ) {
-                perimeterSize++
-            }
+            val outerCorner = isDifferentValue(newPos1, pos) && isDifferentValue(newPos2, pos)
+            val innerCorner = isSameValue(newPos1, pos) && isSameValue(newPos2, pos) && isDifferentValue(newPos3, pos)
 
-            // inner corner
-            else if ((newPos1.withinBounds() && inputList[newPos1.first][newPos1.second] == inputList[pos.first][pos.second])
-                && (newPos2.withinBounds() && inputList[newPos2.first][newPos2.second] == inputList[pos.first][pos.second])
-                && (inputList[newPos3.first][newPos3.second] != inputList[pos.first][pos.second])
-            ) {
+            if (innerCorner || outerCorner) {
                 perimeterSize++
             }
         }
 
 
         directions.forEach { dir ->
-
-            // estimate Area
             val newPos = Pair(pos.first + dir.first, pos.second + dir.second)
             if (newPos !in visited
                 && newPos.withinBounds()
                 && inputList[newPos.first][newPos.second] == inputList[pos.first][pos.second]
             ) {
-                visited.add(newPos)
                 val (newArea, newPerimeter) = getAreaAndPerimeterDiscounted(newPos, visited)
                 areaSize += newArea
                 perimeterSize += newPerimeter
@@ -145,8 +94,24 @@ class Day12(input: String? = null) : Day(12, "Day12", input) {
         }
 
         return areaSize to perimeterSize
-
     }
+
+    private val directions = listOf(
+        -1 to 0,  // up
+        0 to 1,   // right
+        1 to 0,   // down
+        0 to -1   // left
+    )
+
+    private fun Pair<Int, Int>.withinBounds() =
+        first in inputList.indices && second in inputList[first].indices
+
+    private fun isDifferentValue(pos1: Pair<Int, Int>, pos2: Pair<Int, Int>) =
+        !pos1.withinBounds() || inputList[pos1.first][pos1.second] != inputList[pos2.first][pos2.second]
+
+    private fun isSameValue(pos1: Pair<Int, Int>, pos2: Pair<Int, Int>) =
+        pos1.withinBounds() && inputList[pos1.first][pos1.second] == inputList[pos2.first][pos2.second]
+
 }
 
 
